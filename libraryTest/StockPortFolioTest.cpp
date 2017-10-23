1ebf40f19523da0be73d66af2025332b7ec3e592
Juan Palacios <juan.palacios.puyana@gmail.com>
2017-10-23 15:48:27 +0200

    Fix name of apple stock

--- libraryTest/StockPortFolioTest.cpp
+++ libraryTest/StockPortFolioTest.cpp
@@ -16,7 +16,7 @@ TEST_F(AStockPortfolio, isEmptyOnCreation)
 
 TEST_F(AStockPortfolio, isNotEmptyAfterPurchase)
 {
-    portfolio.purchase("APPL", 3);
+    portfolio.purchase("AAPL", 3);
     ASSERT_FALSE(portfolio.empty());
 }
 
@@ -27,21 +27,21 @@ TEST_F(AStockPortfolio, uniqueSymbolCountIsZeroOnCreation)
 
 TEST_F(AStockPortfolio, uniqueSymbolCountIncrementsAfterPurchasingNewSymbol)
 {
-    portfolio.purchase("APPL", 3);
+    portfolio.purchase("AAPL", 3);
     ASSERT_THAT(portfolio.countUnique(), Eq(1));
 }
 
 TEST_F(AStockPortfolio, uniqueSymbolCountOnlyIncrementsOnceAfterPurchasingSameSymbol)
 {
-    portfolio.purchase("APPL", 1);
+    portfolio.purchase("AAPL", 1);
     ASSERT_THAT(portfolio.countUnique(), Eq(1));
-    portfolio.purchase("APPL", 1);
+    portfolio.purchase("AAPL", 1);
     ASSERT_THAT(portfolio.countUnique(), Eq(1));
 }
 
 TEST_F(AStockPortfolio, uniqueSymbolCountIncrementsOncePerNewSymbolPurchase)
 {
-    portfolio.purchase("APPL", 3);
+    portfolio.purchase("AAPL", 3);
     ASSERT_THAT(portfolio.countUnique(), Eq(1));
     portfolio.purchase("IBM", 5);
     ASSERT_THAT(portfolio.countUnique(), Eq(2));
@@ -51,7 +51,7 @@ TEST_F(AStockPortfolio, uniqueSymbolCountIncrementsOncePerNewSymbolPurchase)
 
 TEST_F(AStockPortfolio, hasNoSharesOnCreation)
 {
-    const char* stocks[] = {"APPL", "GOOGL", "IBM", "foo", "bar", "baz"};
+    const char* stocks[] = {"AAPL", "GOOGL", "IBM", "foo", "bar", "baz"};
     for (auto stock: stocks)
     {
         ASSERT_THAT(portfolio.count(stock), Eq(0));
@@ -60,42 +60,42 @@ TEST_F(AStockPortfolio, hasNoSharesOnCreation)
 
 TEST_F(AStockPortfolio, purchasesExpectedAmountOfShares)
 {
-    portfolio.purchase("APPL", 3);
-    ASSERT_THAT(portfolio.count("APPL"), Eq(3));
+    portfolio.purchase("AAPL", 3);
+    ASSERT_THAT(portfolio.count("AAPL"), Eq(3));
 }
 
 TEST_F(AStockPortfolio, countIncrementsOnceAfterSuccessivePurchases)
 {
-    portfolio.purchase("APPL", 1);
-    ASSERT_THAT(portfolio.count("APPL"), Eq(1));
-    portfolio.purchase("APPL", 1);
-    ASSERT_THAT(portfolio.count("APPL"), Eq(2));
+    portfolio.purchase("AAPL", 1);
+    ASSERT_THAT(portfolio.count("AAPL"), Eq(1));
+    portfolio.purchase("AAPL", 1);
+    ASSERT_THAT(portfolio.count("AAPL"), Eq(2));
 }
 
 
 TEST_F(AStockPortfolio, countDecrementsWhenSellingShares)
 {
-    portfolio.purchase("APPL", 5);
-    portfolio.sell("APPL", 3);
-    ASSERT_THAT(portfolio.count("APPL"), Eq(2));
+    portfolio.purchase("AAPL", 5);
+    portfolio.sell("AAPL", 3);
+    ASSERT_THAT(portfolio.count("AAPL"), Eq(2));
 }
 
 TEST_F(AStockPortfolio, countIsZeroAfterSellingAllShares)
 {
-    portfolio.purchase("APPL", 5);
-    portfolio.sell("APPL", 5);
-    ASSERT_THAT(portfolio.count("APPL"), Eq(0));
+    portfolio.purchase("AAPL", 5);
+    portfolio.sell("AAPL", 5);
+    ASSERT_THAT(portfolio.count("AAPL"), Eq(0));
     ASSERT_THAT(portfolio.countUnique(), Eq(0));
     ASSERT_TRUE(portfolio.empty());
 }
 
 TEST_F(AStockPortfolio, cannotSellNonOwnedShares)
 {
-    portfolio.purchase("APPL", 3);
-    ASSERT_THROW(portfolio.sell("APPL", 5), InvalidTradeException);
+    portfolio.purchase("AAPL", 3);
+    ASSERT_THROW(portfolio.sell("AAPL", 5), InvalidTradeException);
 }
 
 TEST_F(AStockPortfolio, cannotSellFromEmptyPortfolio)
 {
-    ASSERT_THROW(portfolio.sell("APPL", 5), InvalidTradeException);
+    ASSERT_THROW(portfolio.sell("AAPL", 5), InvalidTradeException);
 }
